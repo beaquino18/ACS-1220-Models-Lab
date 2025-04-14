2624d6001d33e952378c13d465595115ed5b5f7b
Bernadette Aquino <bernadette.p.aquino@gmail.com>
2025-04-14 15:45:08 -0700

    working python interpreter

--- books_app/config.py
+++ books_app/config.py
@@ -4,9 +4,13 @@ import os
 
 load_dotenv()
 
+# Get the absolute path to the project root directory
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
 class Config(object):
     """Set environment variables."""
 
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    # Use an absolute path to the database file
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(basedir, "books_app", "database.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'xq9YLa6EKV')
